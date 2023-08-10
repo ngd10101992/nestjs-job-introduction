643bbcfc58081822803a30ed44b8f2dfa6ecf212
dat.nguyengia <dat.nguyengia@vti.com.vn>
2023-08-10 18:27:46 +0900

    config jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
