37b6266b7c0380986bb5d0f9f4516e1c04ca633a
dat.nguyengia <dat.nguyengia@vti.com.vn>
2023-08-10 10:35:58 +0900

    hash password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    // MongooseModule.forRoot('mongodb+srv://datnguyengia:QcX4PN8wMAzJoiey@cluster0.dpjkagc.mongodb.net/')
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
