499a5e86339035548c683d93aa3d2fe4c837fc13
dat.nguyengia <dat.nguyengia@vti.com.vn>
2023-08-14 12:40:46 +0900

    handle soft deleted

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { InjectModel } from '@nestjs/mongoose'
-import { User } from './schemas/user.schema'
+import { User, UserDocument } from './schemas/user.schema'
 import mongoose, { Model } from 'mongoose'
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs'
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose'
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+  constructor(@InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>) { }
 
   hashPassword(password: string): string {
     const salt = genSaltSync(10)
@@ -50,6 +51,6 @@ export class UsersService {
     if (!mongoose.Types.ObjectId.isValid(id))
       return 'not found user'
 
-    return this.userModel.deleteOne({ _id: id })
+    return this.userModel.softDelete({ _id: id })
   }
 }
