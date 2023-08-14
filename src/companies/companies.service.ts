8dafd8ceeb629be615fdf7104a8160cbb37d1504
dat.nguyengia <dat.nguyengia@vti.com.vn>
2023-08-14 16:46:06 +0900

    custom User decorator để thay req.user

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,14 +4,20 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class CompaniesService {
-  constructor(@InjectModel(Company.name) private userModel: SoftDeleteModel<CompanyDocument>) { }
+  constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) { }
 
-  async create(createCompanyDto: CreateCompanyDto) {
-    const company = await this.userModel.create({ ...createCompanyDto })
-    return company
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
   }
 
   findAll() {
