694abec63932059132f087ed5a5a13d506c42237
dat.nguyengia <dat.nguyengia@vti.com.vn>
2023-08-14 11:26:47 +0900

    disable guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
 import { AppService } from './app.service'
 import { LocalAuthGuard } from './auth/local-auth.guard'
 import { AuthService } from './auth/auth.service'
-import { JwtAuthGuard } from './auth/jwt-auth.guard'
+import { Public } from './decorator/customize'
 
 @Controller()
 export class AppController {
@@ -11,13 +11,13 @@ export class AppController {
     private authService: AuthService
   ) { }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user)
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core'
+import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
+import { JwtAuthGuard } from './auth/jwt-auth.guard'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+  const reflector = app.get(Reflector)
   const port = configService.get('PORT')
+
+  app.useGlobalGuards(new JwtAuthGuard(reflector))
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(port)
 }
